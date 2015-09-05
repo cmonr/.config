1d145667a608ea31e1ae5d3f207b41335053c5c5
Cruz Monrreal II <Cruz.Monrreal@gmail.com>
2015-09-05 18:31:52 -0500

    Initial commit of major restructure.
    To find other files, reference github.com/cmonr/.dotfiles

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# TODO: If this file is not in ~/Projects/.dotfiles, exit
+
+# Move to base directory
+cd ~/Projects/.config/
+
+#
+# Symlinks, symlinks everywhere!
+#
+
+# Awesome WM
+ln -s {`pwd`,~/.config}/awesome
