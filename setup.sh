a7754914f4abec7fe47d6a792a0bb86068e99177
Cruz Monrreal II <Cruz.Monrreal@gmail.com>
2016-05-30 15:37:50 -0500

    Added symlinks for rules and fstab rules for removable drives

--- setup.sh
+++ setup.sh
@@ -13,3 +13,15 @@ cd ~/Projects/.config/
 ln -s {`pwd`,~/.config}/awesome
 
 # udev rules
+sudo ln -s {`pwd`,/etc/udev/rules}/udev/97-udisks2.rules
+sudo ln -s {`pwd`,/etc/udev/rules}/udev/98-firmware-dev.rules
+sudo ln -s {`pwd`,/etc/udev/rules}/udev/99-unaliwear.rules
+
+# fstab
+sudo cp /etc/fstab ./fstab
+cat fstab.adtl >> ./fstab
+sudo mv /etc/fstab{,.bak}
+sudo mv ./fstab /etc/fstab
+
+# fstab (dirs)
+sudo mkdir -p /media/{TheUSBDrive,MBed-FRDM-K64F}
